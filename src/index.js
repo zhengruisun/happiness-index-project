86320fbe9828f0d0c3f92528c3379d094a8c55d0
JerryS <zsun0510@uw.edu>
2021-03-08 14:49:14 +0800

    add firebase

--- src/index.js
+++ src/index.js
@@ -3,7 +3,20 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import firebase from 'firebase/app'
 
 import './css/main-style.css';
+var firebaseConfig = {
+    apiKey: "AIzaSyAKoCg88TZcBL4Rfl_7pDhE8pn_DfQhHEQ",
+    authDomain: "project-2-zhanz1-6dbd8.firebaseapp.com",
+    projectId: "project-2-zhanz1-6dbd8",
+    storageBucket: "project-2-zhanz1-6dbd8.appspot.com",
+    messagingSenderId: "433935427896",
+    appId: "1:433935427896:web:dc93ecee6cd918f57639cd",
+    measurementId: "G-JDB4WJ7K3N"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
 
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
